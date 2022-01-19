fc3fd8187e14ca9a57bbce639317a105e7d1e512
chichafon <chichafonchichafon@gmail.com>
2022-01-19 19:15:08 +0400

    create tabBarController.

---
+++ HW13 photo albums GIT/Sources/Scene/ViewControllers/AlbumsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumsViewController.swift
+//  HW13 photo albums GIT
+//
+//  Created by 111 on 19.01.2022.
+//
+
+import Foundation
+import UIKit
+
+class AlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Альбомы"
+    }
+}
--- HW13 photo albums GIT/Sources/Scene/ViewControllers/ForYouViewController.swift
+++ HW13 photo albums GIT/Sources/Scene/ViewControllers/ForYouViewController.swift
@@ -1,20 +1,18 @@
 //
-//  ViewController.swift
+//  ForYouViewController.swift
 //  HW13 photo albums GIT
 //
 //  Created by 111 on 19.01.2022.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ForYouViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .white
+        title = "Для вас"
     }
-
-
 }
-
